53cc20ef7bad7dd2a846f9acb51779cc1e03b970
Carvalho, Bernardo (bcar) <bernardo.carvalho@tecnico.ulisboa.pt>
2021-07-26 13:17:42 +0100

    Added files

---
+++ red-pitaya/data_rp_cvs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 09 12:11:30 2021
+
+@author: Bernardo Carvalho
+"""
+
+import sys
+import numpy as np
+# import matplotlib.pyplot as plt
+import wavio
+import pandas as pd
+
+if len(sys.argv) > 1:
+    filename = str(sys.argv[1])
+else:
+    filename = 'data_file_2020-10-09_18-44-32'
+
+NUM_CHANNELS = 32
+
+data_raw = wavio.read(filename + '.wav')
+
+ch0 = data_raw.data
+
+df=pd.DataFrame(ch0, dtype=np.int16, columns=['ch0'])
+df.to_csv(filename + '.csv')
---
+++ red-pitaya/data_rp_plot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 09 12:11:30 2021
+
+@author: Bernardo Carvalho
+"""
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import wavio
+
+if len(sys.argv) > 1:
+    filename = str(sys.argv[1])
+else:
+    filename = 'data_file_2020-10-09_18-44-32.wav'
+
+NUM_CHANNELS = 32
+
+data_raw = wavio.read(filename)
+
+ch0 = data_raw.data
+
+fig, ax = plt.subplots()
+
+ax.plot(ch0, label='Channel 0')
+ax.set_ylabel('LSB')
+ax.set_title("ATCA v2 32 bit data")  # Add a title to the axes.
+ax.legend(loc='upper left')  # Add a legend.
+plt.show()
