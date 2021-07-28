b84c0355d494268f9152c08b7e2b1031664f4f03
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2021-07-28 15:00:14 +0100

    corrected python files

--- red-pitaya/data_rp_plot.py
+++ red-pitaya/data_rp_plot.py
@@ -7,7 +7,7 @@ Created on Mon Sep 09 12:11:30 2021
 """
 
 import sys
-# import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 import wavio
 
@@ -16,16 +16,19 @@ if len(sys.argv) > 1:
 else:
     filename = 'data_file_2020-10-09_18-44-32'
 
-
+DECIMATION = 8
+FREQ_SAMP = 125.0e3/DECIMATION  # (in kHz)
 data_raw = wavio.read(filename + '.wav')
 
-ch0 = data_raw.data
+ch0 = data_raw.data / 2**13
+time = np.arange(ch0.size)/FREQ_SAMP
 
 fig, ax = plt.subplots()
 
-ax.plot(ch0, label='Channel 0')
-ax.set_ylabel('LSB')
-ax.set_title("ATCA v2 32 bit data")  # Add a title to the axes.
+ax.plot(time, ch0, label='Channel 0')
+ax.set_ylabel('V / V')
+ax.set_xlabel('Time/ms')
+ax.set_title("Red Pitaya Kistler")  # Add a title to the axes.
 ax.legend(loc='upper left')  # Add a legend.
-plt.savefig(filename +'.png')
+plt.savefig(filename + '.png')
 plt.show()
