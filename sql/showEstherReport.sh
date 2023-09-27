1c217fc740423311bf185726afab7b464a9ac1de
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2023-09-27 18:55:53 +0100

    Updated script, showing reports
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

---
+++ sql/showEstherReport.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Install a virtual environmet with
+# python3 -m venv --system-site-packages ~/.local/venvs/python-epics
+# Install pyepics with:
+# ~/.local/venvs/python-epics/bin/python -m pip install pyepics
+# Run script with:
+~/.local/venvs/python-epics/bin/python showEstherReport.py
