49368af82e983f3d7c0a968b2e1fd1bb2c91fe3a
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2023-10-26 00:24:36 +0100

    Added PDF report
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- sql/checkListSql.py
+++ sql/checkListSql.py
@@ -6,6 +6,8 @@ PyQt6 SQL App for signed Esther Checklists
 #import os
 import sys
 
+from reportlab.pdfgen import canvas
+
 from PyQt6.QtCore import QSize, Qt, QSortFilterProxyModel
 # from PyQt6.QtSql import QSqlDatabase, QSqlTableModel
 from PyQt6.QtGui import QFont
@@ -52,7 +54,8 @@ CHECK_LIST_QUERY = ("SELECT CheckLineId, ChecklistName, EstherRoles.RoleName, "
             "ORDER BY LineOrder ASC"
         )
 
-CHECKLINE_LAST_QUERY = ("SELECT CheckLine, ChecklistLines.LineOrder, LineStatusDate, ChecklistLines.LineDesc, CheckLineSigned.SignedBy, EstherRoles.RoleName "
+CHECKLINE_LAST_QUERY = ("SELECT CheckLine, ChecklistLines.LineOrder, LineStatusDate, ChecklistLines.LineDesc, "
+            "EstherRoles.RoleName, checkValue "
             "FROM CheckLineSigned "
             "INNER JOIN ChecklistLines ON CheckLineSigned.CheckLine = ChecklistLines.CheckLineId "
             "INNER JOIN EstherRoles ON ChecklistLines.SignedBy = EstherRoles.RoleId "
@@ -93,9 +96,14 @@ class SignDialog(QDialog):
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
 
+        self.rBv = QRadioButton('No Problem', self)
+        self.rBv.setChecked(True)
+        self.rBa = QRadioButton('Attention required', self)
         self.layout = QVBoxLayout()
         message = QLabel("Sure you want to sign this Checkline?")
         self.layout.addWidget(message)
+        self.layout.addWidget(self.rBv)
+        self.layout.addWidget(self.rBa)
         self.layout.addWidget(self.buttonBox)
         self.setLayout(self.layout)
 
@@ -167,6 +175,8 @@ class MainWindow(QMainWindow):
         layoutTools = QVBoxLayout()
 
         refreshButt = QPushButton("Refresh")
+        pdfButt = QPushButton("Report PDF")
+        pdfButt.clicked.connect(self.report_pdf)
 #        refreshButt.clicked.connect(self.refresh_model)
 
 #        layoutTools.addWidget(add_rec)
@@ -175,6 +185,7 @@ class MainWindow(QMainWindow):
 #        self.list.textChanged.connect(self.update_query)
         
         layoutTools.addWidget(refreshButt)
+        layoutTools.addWidget(pdfButt)
         layoutTools.addWidget(QLabel('Exp. Phase'))
         widget = QComboBox()
         widget.addItems(["StartOfDay", "Shot", "EndOfDay"])
@@ -341,17 +352,6 @@ class MainWindow(QMainWindow):
         model = self.tableWaitOK.model()
         query = model.query()
         # queryWaitOK = QSqlQuery(db=db)
-        """
-        queryWaitOK.prepare(
-            "SELECT CheckLineId, LineOrder, LineDesc, SignedBy "
-            "FROM ChecklistLines "
-            "WHERE LineOrder > :l_order AND Checklist = :list_id AND SignedBy = :sign_by "
-            #"WHERE Checklist = :list_id AND ChiefEngineer = :ce_checked AND Researcher = :re_checked "
-            "ORDER BY LineOrder ASC LIMIT 3"
-        )
-        #queryWaitOK.bindValue(":l_order", lastOK)
-        """
-
         query.bindValue(":l_order", self.lastSigned)
         query.bindValue(":list_id", self.listId)
         query.bindValue(":sign_by", self.signBy)
@@ -417,19 +417,29 @@ class MainWindow(QMainWindow):
 
         insertCLine = QSqlQuery(db=db)
         insertCLine.prepare("INSERT INTO CheckLineSigned VALUES (NULL, :shot_no, "
-                "current_timestamp(), :cLine_id, :sign_by, 0, NULL)")
+                            "current_timestamp(), :cLine_id, :sign_by, :sign_val, NULL)")
         insertCLine.bindValue(":shot_no", self.shotNo)
         insertCLine.bindValue(":cLine_id", self.nextLineId)
         insertCLine.bindValue(":sign_by", self.signBy)
         # self.insertCLine.bindValue(":cLine_id", 6)
         dlg = SignDialog(self)
         if dlg.exec():
+            if dlg.rBv.isChecked():
+                signVal = 0
+            elif dlg.rBa.isChecked():
+                signVal = 1
+            else:
+                signVal = 255
+            print(f"dlg rBv: {signVal}")
+            insertCLine.bindValue(":sign_val", signVal)
             #print("Success! " + str(self.nextLineId))
             if insertCLine.exec():
+                QMessageBox.information(None, "Record Inserted ", f"dlg rBv: {signVal}")
                 print("Inserted record")
                 self.update_queryLastCL()
                 self.update_queryWaitOK()
             else:
+                QMessageBox.critical(None, "Record NOT Inserted ", f"dlg rBv: {signVal}")
                 #print("NOT Inserted")
                 print("Insert failed: " + insertCLine.lastQuery())
                 print(f":shot_no + {self.shotNo} :cLine_id {self.nextLineId} :sign_by {self.signBy}")
@@ -437,6 +447,29 @@ class MainWindow(QMainWindow):
             print("Cancel!")
 
         #insertCLine.bindValue(":sign_by", self.shotNo)
+    def report_pdf(self):
+        # Create the report
+        pdf = canvas.Canvas(f"report_{self.shotNo}.pdf")
+        pdf.setFont("Helvetica-Bold", 16)
+        pdf.drawString(50, 750, "Customer Report")
+        pdf.setFont("Helvetica", 12)
+        query = QSqlQuery(db=db)
+        query.prepare(CHECKLINE_LAST_QUERY)
+        #qryModel.setQuery(query)
+        #self.tableLastCL.setModel(qryModel)
+        query.bindValue(":shot_no", self.shotNo)
+        query.bindValue(":sign_by", self.signBy)
+        query.bindValue(":list_id", self.listId)
+        if (query.exec()):
+            y = 700
+            while query.next():
+                pdf.drawString(50, y, f"Name: {query.value(0)}")
+                pdf.drawString(50, y-20, f"Description: {query.value(3)}")
+                y -= 60
+        pdf.save()
+
+    #    self.model.select()
+
 
 #    def update_filter(self, s):
 #        filter_str = 'Checklist LIKE "{}"'.format(s)
