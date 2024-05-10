f3b07cd772ee4068a264f130d05ed24a53e16ee9
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2024-05-10 17:33:15 +0100

    Added systemd service
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- sql/gxPumpEpicsPut.py
+++ sql/gxPumpEpicsPut.py
@@ -15,6 +15,7 @@ Script to extract temperature valuesi from Edwards GX Pumps
 import re
 import requests
 from bs4 import BeautifulSoup
+import signal
 
 import time
 from epics import caput  # caget, caput, cainfo
@@ -26,18 +27,30 @@ PVs = [{'pv': 'ST', 'ip': '192.168.0.41'},
        {'pv': 'DT', 'ip': '192.168.0.42'}]
 
 
+class GracefulKiller:
+    kill_now = False
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum, frame):
+        self.kill_now = True
+
+
 def get_temp(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     element = soup.find('script', attrs={'type': 'text/javascript'})
     ele_str = element.string.strip()
 #    print(ele_str)
-    res = re.findall(r"self.parent.ObjectGaugeUpdate +\(57, +\'number\', +\'(\d+)\'",
-                     ele_str)
-    resHz = re.findall(r"self.parent.ObjectGaugeUpdate"
-                       r" +\(184, +\'number\', +\'(\d+)\'", ele_str)
-    print(f"Freq: {int(resHz[0])} Hz")
-    return int(res[0])
+    res = re.findall(r"self.parent.ObjectGaugeUpdate "
+                     r"+\(57, +\'number\', +\'(\d+)\'", ele_str)
+    # resHz = re.findall(r"self.parent.ObjectGaugeUpdate"
+    #                   r" +\(184, +\'number\', +\'(\d+)\'", ele_str)
+    # print(f"Freq: {int(resHz[0])} Hz")
+    temp = int(res[0])
+    return temp
 
 
 def get_url(ip):
@@ -46,17 +59,20 @@ def get_url(ip):
 # browser.open("http://192.168.0.41/sev_gauges.htm")
 
 
+def clear_pvs():
+    for p in PVs:
+        pv_name = PV_NAME.format(p['pv'])
+        caput(pv_name, 'nan')
+
+
 def read_loop():
-    while True:
+    killer = GracefulKiller()
+    while not killer.kill_now:
         for p in PVs:
             url = get_url(p['ip'])
             pv_name = PV_NAME.format(p['pv'])
             try:
                 temp = get_temp(url)
-            except KeyboardInterrupt:
-                print("KeyboardInterrupt. Stopping")
-                caput(pv_name, 'nan')
-                exit()
             except Exception:
                 caput(pv_name, 'nan')
                 print("Can't get page")
@@ -64,8 +80,13 @@ def read_loop():
             print(f"{pv_name} Temper {temp} ºC")
         time.sleep(30)  # Sleep seconds
 
+    # print("KeyboardInterrupt. Stopping")
+    clear_pvs()
+    print("End of the program gxPumpEpicsPut.py. Killed gracefully :)")
+
 
 if __name__ == '__main__':
+    clear_pvs()
     read_loop()
 
 # vim: set syntax=python ts=4 sw=4 sts=4 sr et
