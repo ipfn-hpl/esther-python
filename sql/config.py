29359547984b40e97de3814b096b46d8e3c1c4c5
Bernardo Brotas Carvalho <14007721+bernardocarvalho@users.noreply.github.com>
2024-07-31 16:37:35 +0100

    Update config.py

--- sql/config.py
+++ sql/config.py
@@ -1,5 +1,5 @@
 # git update-index --assume-unchanged config.py
-host = "epics.ipfn.tecnico.ulisboa.pt"
+host = "esther.tecnico.ulisboa.pt"
 database = "esther_report"
 username = "xxxx"
 password = "xxxx"
