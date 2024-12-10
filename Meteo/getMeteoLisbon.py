c5d36fab53bf95b76015d4d34d33372935caf5e7
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2024-12-10 16:49:11 +0000

    Added Auto Kistler reset. New Meteo script
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

---
+++ Meteo/getMeteoLisbon.py
@@ -0,0 +1,42 @@
+"""
+
+"idEstacao": 1210762,
+"localEstacao": "Lisboa, Tapada da Ajuda",
+
+"idEstacao": 7240919,
+"localEstacao": "Lisboa, Amoreiras (Lic. Franc\u00eas)",
+
+"idEstacao": 1200535,
+"localEstacao": "Lisboa (Geof\u00edsico)",
+
+"idEstacao": 1200579,
+"localEstacao": "Lisboa (G.Coutinho)",
+
+"""
+import geojson
+import urllib.request
+# import pprint as pp
+
+IPMA_URL = 'https://api.ipma.pt/open-data/observation/meteorology/stations/obs-surface.geojson'
+file = 'obs-surface.geojson'
+with open(file, 'r') as file:
+    gj2 = geojson.load(file)
+
+with urllib.request.urlopen(IPMA_URL) as url:
+    gj = geojson.load(url)
+    # data = json.load(url)
+# Now, let's print the GeoJSON data to understand its structure
+# pp.pprint(dict(gj))
+features = gj['features']
+printLoc = True
+for feat in features:
+    prop = feat['properties']
+    if feat['properties']['idEstacao'] == 1200579:
+        if printLoc:
+            printLoc = False
+            print(feat['properties']['localEstacao'])
+
+        print("time %s" % prop['time'], end='')
+        print(", temperatura %.2f ºC" % prop['temperatura'], end='')
+        print(", pressao %.2f mBar" % prop['pressao'], end='')
+        print(", humidade %.1f%%" % prop['humidade'])
--- pulse-schedule/pulse-start.py
+++ pulse-schedule/pulse-start.py
@@ -6,9 +6,66 @@ https://stackoverflow.com/questions/8241099/executing-tasks-in-parallel-in-pytho
 
 import argparse
 import serial
+import io
 import time
 from threading import Thread
 import subprocess
+
+
+class kistlerCom():
+    """
+    Serial connection to Kistler 5015 on CC
+    """
+
+    def __init__(self, serial_port='/dev/ttyUSB0', baud_rate=115200,
+                 read_timeout=1):
+        """
+        Initializes the serial connection to the Arduino board
+        """
+        self.ser = serial.Serial(serial_port, baud_rate, timeout=read_timeout)
+        self.sio = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser),
+                                    newline="\r")
+
+    def measureReset(self):
+        """
+        """
+        # command = "RO0\r"
+        # self.sio.write(unicode(command))
+        self.sio.write("RO0\n")
+        self.sio.flush()  # it is buffering. required to get the data out *now*
+        time.sleep(0.1)
+        command = "RO1\r"
+        self.sio.write("RO1\n")
+        self.sio.write(command)
+        self.sio.flush()  # it is buffering. required to get the data out *now*
+        time.sleep(0.1)
+        line = self.sio.readline()   # read a '\n' terminated line
+        print(f"Kistler reply: {line}")
+        return line
+
+    def measureActive(self, active=False):
+        """
+        Unicode strings must be encoded (e.g. 'hello'.encode('utf-8').
+        """
+        if active:
+            # command = "RO1\r".encode('utf-8')
+            command = "RO1\r".encode('utf-8')
+        else:
+            command = "RO0\r".encode('utf-8')
+        # print(command)
+        self.sio.write(command)
+        self.sio.flush()  # it is buffering. required to get the data out *now*
+        time.sleep(0.5)
+        line = self.sio.readline()   # read a '\n' terminated line
+        print(f"reply: {line}")
+        return line
+
+    def close(self):
+        """
+        To ensure we are properly closing our connection to the
+        Arduino device.
+        """
+        self.ser.close()
 
 
 class quantel():
@@ -17,12 +74,14 @@ class quantel():
     """
 
     def __init__(self, serial_port='/dev/ttyS0', baud_rate=9600,
-                 read_timeout=20):
+                 read_timeout=2):
         """
         Initializes the serial connection to the Arduino board
         """
-        self.ser = serial.Serial(serial_port, baud_rate)
-        self.ser.timeout = read_timeout  # Timeout for readline()
+        self.ser = serial.Serial(serial_port, baud_rate, timeout=read_timeout)
+        # self.ser.timeout = read_timeout  # Timeout for readline()
+        self.sio = io.TextIOWrapper(io.BufferedRWPair(self.ser, self.ser),
+                                    newline="\r\n")
 
     def standBy(self):
         """
@@ -36,6 +95,25 @@ class quantel():
         print(f"reply: {line}")
         return line
 
+    def armLaser(self):
+        """
+        """
+        command = "S\r\n".encode('utf-8')
+        self.ser.write(command)
+        time.sleep(0.1)
+        command = "Q010101\r\n".encode('utf-8')
+        self.ser.write(command)
+        time.sleep(0.1)
+        command = "A\r\n".encode('utf-8')
+        self.ser.write(command)
+        time.sleep(0.1)
+        command = "QI\r\n".encode('utf-8')
+        self.ser.write(command)
+        time.sleep(0.1)
+        line = self.ser.readline()   # read a '\n' terminated line
+        print(f"Quantel reply: {line}")
+        return line
+
     def triggerQSwitch(self):
         """
         Unicode strings must be encoded (e.g. 'hello'.encode('utf-8').
@@ -44,7 +122,7 @@ class quantel():
         self.ser.write(command)
         time.sleep(0.5)
         line = self.ser.readline()   # read a '\n' terminated line
-        print(f"reply: {line}")
+        print(f"Quantel reply: {line}")
         return line
 
     def close(self):
@@ -62,13 +140,25 @@ def parse_args():
 
     parser.add_argument('-r', '--host_rp', default='10.10.136.229',
                         help='Red Pitay IP address')
+    parser.add_argument('-a', '--laserArm',
+                        action='store_true', help='Arm Quantel Laser')
     parser.add_argument('-s', '--laserStandby',
                         action='store_true', help='Set Quantel in Standby')
     parser.add_argument('-c', '--redpitayaConfig',
                         action='store_true', help='Check Red Pitaya Config')
     parser.add_argument('-f', '--fire',
                         action='store_true', help='Fire Quantel')
+    parser.add_argument('-k', '--kistler',
+                        action='store_true', help='Reset Kistler')
     return parser.parse_args()
+
+
+def taskResetKistler():
+    kt = kistlerCom()
+    reply = kt.measureReset()
+    kt.close()
+    time.sleep(2)
+    print("Task 1 Finished")
 
 
 def task1():
@@ -76,19 +166,11 @@ def task1():
     print("Task 1 Finished")
 
 
-def taskFireQuantel():
-    qt = quantel()
-    time.sleep(0.5)
-    reply = qt.triggerQSwitch()
-    qt.close()
-    print(reply)
-    print("Trigger Quantel Finished")
-
-
-def taskRPitayaStart(host="10.10.136.229"):
+def taskRPitayaStart(host="10.10.136.232"):
     # list_files = subprocess.run(["ls", "-l"])
     # list_files = subprocess.run(["ls", "-l"], stdout=subprocess.DEVNULL)
     # print("The exit code was: %d" % list_files.returncode)
+    time.sleep(0.5)
     rp_query = subprocess.run(["/home/esther/.local/bin/rpsa_client",
                                "--remote",
                                "--mode=start",
@@ -96,7 +178,16 @@ def taskRPitayaStart(host="10.10.136.229"):
     print("RP Start exit code was: %d" % rp_query.returncode)
 
 
-def checkRPitayaConfig(host="10.10.136.229"):
+def taskFireQuantel():
+    qt = quantel()
+    time.sleep(1.0)
+    reply = qt.triggerQSwitch()
+    qt.close()
+    print(reply)
+    print("Trigger Quantel Finished")
+
+
+def checkRPitayaConfig(host="10.10.136.232"):
     # list_files = subprocess.run(["ls", "-l"], stdout=subprocess.DEVNULL)
     # print("The exit code was: %d" % list_files.returncode)
     rp_query = subprocess.run(["/home/esther/.local/bin/rpsa_client",
@@ -116,17 +207,19 @@ def task5():
 
 
 def firePulse():
-    t1 = Thread(target=task1)
-    t2 = Thread(target=taskRPitayaStart)
-    t3 = Thread(target=taskFireQuantel)
+    print("Firing Pulse")
+    t0 = Thread(target=taskResetKistler)
+    # t1 = Thread(target=task1)
+    t1 = Thread(target=taskRPitayaStart)
+    t2 = Thread(target=taskFireQuantel)
 
+    t0.start()
     t1.start()
     t2.start()
-    t3.start()
 
+    t0.join()
     t1.join()
     t2.join()
-    t3.join()
 
 
 if __name__ == '__main__':
@@ -136,6 +229,20 @@ if __name__ == '__main__':
         qt = quantel()
         reply = qt.standBy()
         qt.close()
+        print(reply)
+        exit()
+
+    if args.laserArm:
+        qt = quantel()
+        reply = qt.armLaser()
+        qt.close()
+        print(reply)
+        exit()
+
+    if args.kistler:
+        kt = kistlerCom()
+        reply = kt.measureReset()
+        kt.close()
         print(reply)
         exit()
 
